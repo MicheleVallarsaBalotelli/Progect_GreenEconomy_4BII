e634b98b600e7ed6778ec1ceb8eae05577c213db
mkelino <michivallarsa07@gmail.com>
2026-04-07 14:07:53 +0200

    Aggiunta classe JsonService per salvataggio dati in JSON

---
+++ Progect_GreenEconomy_4BII/JsonService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Progect_GreenEconomy_4BII
+{
+    public class JsonService
+    {
+        private static string nomeFile = "dataset_ambientale.json";
+
+        public static void SalvaDati(List<EnviromentData> lista)
+        {
+            string json = JsonSerializer.Serialize(lista, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(nomeFile, json);
+        }
+
+        public static List<EnviromentData> CaricaDati()
+        {
+            if (!File.Exists(nomeFile))
+            {
+                return new List<EnviromentData>();
+            }
+
+            // leggo tutto il testo 
+            string json = File.ReadAllText(nomeFile);
+
+            // trasformo il testo JSON in una lista di oggetti EnviromentData
+            List<EnviromentData> listaCaricata = JsonSerializer.Deserialize<List<EnviromentData>>(json);
+
+            return listaCaricata;
+        }
+    }
+}
